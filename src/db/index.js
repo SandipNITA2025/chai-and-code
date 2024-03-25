6a21b859f96efc9f077ad026d41ea38c1bd4d472
SandipNITA2025 <sarkarsandip7910@gmail.com>
2024-03-25 17:45:05 +0530

    utils

--- src/db/index.js
+++ src/db/index.js
@@ -6,9 +6,9 @@ export const connectDB = async () => {
     const connection = await mongoose.connect(
       `${process.env.DATABASE_URL}/${DB_NAME}`
     );
-    console.log(`DATABASE connected ! HOST : ${connection.connection.host}`);
+    console.log(`DATABASE Connected ! HOST : ${connection.connection.host}`);
   } catch (error) {
-    console.log("DATABASE connection error :", error);
+    console.log("DATABASE Connection error :", error);
     process.exit(1);
     // throw error;
   }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import 'dotenv/config'
+import "dotenv/config";
 import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server running on port : ${process.env.PORT || 8080}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed!", err);
+  });
